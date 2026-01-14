18cf6b9a6e1ab83d1bff8939f306c36ad800a8e3
nik2005 <nikhilbhaskar2005@gmail.com>
2026-01-14 13:09:15 +0530

    perf(storage): migrate to append-only log structure for O(1) writes

--- src/lib.rs
+++ src/lib.rs
@@ -1,41 +1,64 @@
 use std::collections::HashMap;
-use std::fs::{self, OpenOptions};
+use std::fs::{self, File, OpenOptions};
+use std::io::{self,BufReader, BufWriter, Write};
 use std::path::PathBuf;
 use serde::{Serialize, Deserialize};
+use serde_json::Deserializer;
 
-#[derive(Serialize, Deserialize, Debug)]
 pub struct KvStore{
 	map: HashMap<String,String>,
-	#[serde(skip)]
-	path: PathBuf,
+	writer: BufWriter<File>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+enum Command{
+	Set {key: String, value: String},
+	Remove{key: String},
 }
 
 impl KvStore{
 	pub fn open(path: PathBuf) -> std::io::Result<KvStore>{
-		let file_result = fs::read_to_string(&path);
+		let file = OpenOptions::new()
+			.create(true)
+			.write(true)
+			.append(true)
+			.read(true)
+			.open(&path)?;
 		
-		match file_result{
-			Ok(contents) => {
-				let mut store: KvStore = serde_json::from_str(&contents)
-							.unwrap_or_else(|_| KvStore {
-							map: HashMap::new(),
-							path: path.clone(),
-				});
-			store.path = path;
-			Ok(store)
-			},
-			Err(_) => {
-				Ok(KvStore {
-					map: HashMap::new(),
-					path,
-				})
+		let mut map = HashMap::new();
+		
+		let reader = BufReader::new(File::open(&path)?);
+		
+		let stream = Deserializer::from_reader(reader).into_iter::<Command>();
+		
+		for command in stream{
+			match command?{
+				Command::Set{key, value} => {
+					map.insert(key, value);
+				}
+				Command::Remove{key} => {
+					map.remove(&key);
+				}
 			}
 		}
+		
+		Ok(KvStore {
+			map,
+			writer: BufWriter::new(file),
+		})
 	}
 	
 	pub fn set(&mut self, key: String, value: String)->std::io::Result<()>{
+		let cmd = Command::Set{
+			key:key.clone(),
+			value: value.clone()
+		};
+		
+		serde_json::to_writer(&mut self.writer, &cmd)?;
+		self.writer.flush()?;
+		
+		
 		self.map.insert(key,value);
-		self.save()?;
 		Ok(())
 	}
 	
@@ -44,17 +67,18 @@ impl KvStore{
 	}
 	
 	pub fn remove(&mut self,key: String)-> std::io::Result<()>{
-		self.map.remove(&key);
-		self.save()?;
-		Ok(())
+		if self.map.contains_key(&key){
+			let cmd = Command::Remove{ key:key.clone()};
+			
+			serde_json::to_writer(&mut self.writer, &cmd)?;
+			self.writer.flush()?;
+			
+			self.map.remove(&key);
+			Ok(())
+		}else{
+			Err(io::Error::new(io::ErrorKind::NotFound, "key not found"))
+		}
 	}
-	
-	fn save(&self) -> std::io::Result<()>{
-		let contents = serde_json::to_string_pretty(&self)?;
-		fs::write(&self.path, contents)?;
-		Ok(())
-	}
-	
 }
 
 #[derive(Debug, Serialize, Deserialize)]
